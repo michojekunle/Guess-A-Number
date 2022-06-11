83183d1956fd24bcd4bb5249c5ffaf8464327b80
michojekunle <michojekunle1@gmail.com>
2022-06-11 19:17:45 +0100

    made changes to js file

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ btn.addEventListener('click', function() {
     
     if(input == number ){
         output.innerHTML = `Correct Guess, your guess was ${number}`; 
-    } else if(input < number ){
+    } else if(input > 0 && input < number ){
         output.innerHTML = `Try Again, your guess is too low`; 
-    } else if(input > number ){
+    } else if(input > number && input <= 20){
         output.innerHTML = `Try Again, your guess is too high`; 
     } else{
         output.innerHTML = `Enter a valid number between 1 and 20`; 
