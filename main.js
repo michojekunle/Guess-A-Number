da0083c1378ca038b5b5e01a410d4036d4a93cfd
Ayo Mich <90222287+michojekunle@users.noreply.github.com>
2022-06-11 18:15:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('guess');
+const output = document.getElementById('outputText');
+
+const number = Math.ceil(Math.random()*20);
+
+btn.addEventListener('click', function() {
+    let input = document.getElementById('input').value;
+    
+    if(input == number ){
+        output.innerHTML = `Correct Guess, your guess was ${number}`; 
+    } else if(input < number ){
+        output.innerHTML = `Try Again, your guess is too low`; 
+    } else if(input > number ){
+        output.innerHTML = `Try Again, your guess is too high`; 
+    } else{
+        output.innerHTML = `Enter a valid number between 1 and 20`; 
+    }    
+})
+
